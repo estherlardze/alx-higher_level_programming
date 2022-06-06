357e3c21493e22b8d26e388d1a35adb86617835a
Esther <99372812+EstherLard@users.noreply.github.com>
2022-06-06 22:08:51 +0000

    remove all cC from a string

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    tmp_str = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            tmp_str += i
-    return (tmp_str)
-# return my_string.translate({ord(c): None for c in "cC"})
+    results = my_string.translate({ord(i): None for i in 'cC'})
+    return results
