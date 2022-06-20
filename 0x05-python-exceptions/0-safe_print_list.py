c4d595a6249ffd1fc7c398999a55c21e8c5d7d52
EstherLard <estherlardze8@gmail.com>
2022-06-20 13:54:31 -0700

    print list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    num = 0
+    while True:
+        try:
+            if num < x:
+                print(my_list[num], end='')
+                num += 1
+            else:
+                print()
+                return num
+        except IndexError:
+            print()
+            return num
