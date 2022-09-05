154e39d50da38fcb4d8dd19061d97e9b8f282868
Esther <99372812+EstherLard@users.noreply.github.com>
2022-09-05 15:25:26 -0700

    task-3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-
-if (process.argv.length ===null) {
+if (process.argv.length === 0) {
   console.log('No argument');
 } else if (process.argv.length === 1) {
   console.log('Argument found');
