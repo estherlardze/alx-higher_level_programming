064ac3f7b9862c741b4babd3d76965e06355821c
Esther <99372812+EstherLard@users.noreply.github.com>
2022-09-27 08:00:37 -0700

    network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""A script that
-fetches https://intranet.hbtn.io/status.
-"""
-
+''' fetches https://intranet.hbtn.io/status '''
+from urllib.request import urlopen
 
 if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as resp:
-        content = resp.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
-
+    url = 'https://intranet.hbtn.io/status'
+    with urlopen(url) as result:
+        output = result.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(output)))
+        print('\t- content: {}'.format(output))
+        print('\t- utf8 content: {}'.format(output.decode('utf-8')))
