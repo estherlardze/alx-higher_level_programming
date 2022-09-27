6c0bed7b5e4d53dc3ef43568c5aff92ac98301dd
Esther <99372812+EstherLard@users.noreply.github.com>
2022-09-27 16:58:18 -0700

    task4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-''' GET request to https://intranet.hbtn.io/status using requests module '''
+'''
+Script that fetches https://intranet.hbtn.io/status
+'''
 import requests
 
-if __name__ == '__main__':
-    res = requests.get('https://intranet.hbtn.io/status')
+if __name__ == "__main__":
+    url = requests.get('https://intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type: {}'.format(type(res.text)))
-    print('\t- content: {}'.format(res.text))
+    print('\t- type: {}'.format(type(url.text)))
+    print('\t- content: {}'.format(url.text))
